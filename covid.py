72efc7ba61ea3554eeead54abc112dd293df3a0e
shivam18u <44138380+shivam18u@users.noreply.github.com>
2021-05-06 13:34:40 +0530

    Update covid.py

--- covid.py
+++ covid.py
@@ -3,7 +3,7 @@ import json
 import os
 import time
 
-url='https://cdn-api.co-vin.in//api/v2/appointment/sessions/public/calendarByDistrict?district_id=366&date=06-05-2021'
+url='https://cdn-api.co-vin.in//api/v2/appointment/sessions/public/calendarByDistrict?district_id=366&date=06-05-2021'   ### date and district code
 header={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', 'Accept': 'application/json, text/plain, */*','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Origin': 'https://www.cowin.gov.in','Referer': 'https://www.cowin.gov.in/'}
 unavailable=True
 while(unavailable):
@@ -14,7 +14,7 @@ while(unavailable):
             if (j['available_capacity']>0):
                 print(i['name'],j['available_capacity'])
                 unavailable=False
-                os.startfile('E:\\MUSIC\\RISE I Worlds 2018 - League of Legends.mp3')
+                os.startfile('E:\\MUSIC\\RISE I Worlds 2018 - League of Legends.mp3')    ### Make sure you use \\ for defining path in windows
                 break
     time.sleep(2)
     print('running....')
